d4e420b96d8012b913c42d3d49b548a1005560ab
reza-eftekhar <eftekhar.reza@gmail.com>
2023-10-16 22:05:29 -0700

    auton path in diffrent file

--- src/autons/path_movement.cpp
+++ src/autons/path_movement.cpp
@@ -4,12 +4,13 @@
 #include "autons/chassis.hpp"
 
 // constants for the speed of the robot
+
 const int DRIVE_SPEED = 100;
-int INTAKE_DURATION{5000}; // duration of intake before stopping
+int INTAKE_DURATION{1000}; // duration of intake before stopping
 int INTAKE_SPEED{150}; // for the speed of the intakezzz
 // Function to grab the ball
 void moveForward(double inches,int speed) {
-  chassis.set_drive_pid(inches, speed);
+  chassis.set_drive_pid(inches*2, speed);
   chassis.wait_drive();
 }
 void moveBackward(double inches, int speed) {
@@ -18,35 +19,79 @@ void moveBackward(double inches, int speed) {
 }
 // the move parameter is if moveing forward would help grab the ball for example if the ball if against a barrier, the same for realeing the ball
 void grabBall(int speed, int duration, bool move, int dis) {
-  intake.move(speed);
-  intake2.move(-speed);
-  if(move){
-    moveForward(dis,DRIVE_SPEED);
-  }
-  pros::delay(duration);
-  intake.move(0);
-  intake2.move(0);
-}
-
-void releaseBall(int speed, int duration,bool move, int dis) {
   intake.move(-speed);
   intake2.move(speed);
   if(move){
     moveForward(dis,DRIVE_SPEED);
   }
-  pros::delay(duration);
+  /*pros::delay(duration);
   intake.move(0);
-  intake2.move(0);
+  intake2.move(0);*/
+}
+void stopIntake(){
+	intake.move(0);
+  	intake2.move(0);
 }
 
 
-void turn(int speed, int degrees) {
-  chassis.set_turn_pid(degrees, speed);
-  chassis.wait_drive();
+void releaseBall(int speed, int duration,bool move, int dis) {
+  intake.move(speed);
+  intake2.move(-speed);
+  pros::delay(1000);
+  if(move){
+    moveForward(dis,DRIVE_SPEED);
+  }
+  /*pros::delay(duration);
+  intake.move(0);
+  intake2.move(0);*/
 }
-void autonomousPath() {
-  moveForward(2, DRIVE_SPEED);
+
+
+void swing(int speed, int degrees, bool right) {
+  
+  if(right){
+  	
+  	chassis.set_swing_pid(ez::RIGHT_SWING, degrees, speed);
+  	chassis.wait_drive();
+  }
+  else{
+	chassis.set_swing_pid(ez::LEFT_SWING, degrees, speed);
+  	chassis.wait_drive();
+  }
+}
+
+void  turn(int speed, int degrees, bool right) {
+  
+  if(right){
+  	
+  	chassis.set_turn_pid(-degrees, speed);
+  	chassis.wait_drive();
+  }
+  else{
+	chassis.set_turn_pid(degrees, speed);
+  	chassis.wait_drive();
+  }
+}
+int dd = 0;
+void autonomousPath1() {
+  moveForward(40, DRIVE_SPEED);
   releaseBall(INTAKE_SPEED, INTAKE_DURATION, false, 0);
+  stopIntake();
+  moveBackward(25, DRIVE_SPEED );
+  turn(50, 180, true);
+  moveBackward(40, DRIVE_SPEED);
+
+  //moveBackward(15, DRIVE_SPEED);
+  //turn(50, 45, true);
+  /*grabBall(INTAKE_SPEED, INTAKE_DURATION, false, 0);
+  moveForward(180, DRIVE_SPEED);
+  stopIntake();
+  turn(50, 135, false);
+  moveForward(40, DRIVE_SPEED);
+  releaseBall(INTAKE_SPEED, INTAKE_DURATION, false, 0);
+  moveForward(10, DRIVE_SPEED);
+  pros::delay(1000);
+  return;
   //moveForward(10,DRIVE_SPEED);
   //moveBackward(10,DRIVE_SPEED);
   /*turn(50, -45);
@@ -66,3 +111,8 @@ void autonomousPath() {
 
 
 }
+
+void autonomousPath2() {
+  // the code for the second autonomous path woudl go here
+  }
+
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,77 @@ using namespace Controller;
 
 
 
+/*const int DRIVE_SPEED = 100;
+int INTAKE_DURATION{1000}; // duration of intake before stopping
+int INTAKE_SPEED{150}; // for the speed of the intakezzz
+// Function to grab the ball
+void moveForward(double inches,int speed) {
+  chassis.set_drive_pid(inches*2, speed);
+  chassis.wait_drive();
+}
+void moveBackward(double inches, int speed) {
+  chassis.set_drive_pid(-inches, speed);
+  chassis.wait_drive();
+}
+// the move parameter is if moveing forward would help grab the ball for example if the ball if against a barrier, the same for realeing the ball
+void grabBall(int speed, int duration, bool move, int dis) {
+  intake.move(-speed);
+  intake2.move(speed);
+  if(move){
+    moveForward(dis,DRIVE_SPEED);
+  }
+  /*pros::delay(duration);
+  intake.move(0);
+  intake2.move(0);
+}
+void stopIntake(){
+	intake.move(0);
+  	intake2.move(0);
+}
+
+
+void releaseBall(int speed, int duration,bool move, int dis) {
+  intake.move(speed);
+  intake2.move(-speed);
+  pros::delay(1000);
+  if(move){
+    moveForward(dis,DRIVE_SPEED);
+  }
+  /*pros::delay(duration);
+  intake.move(0);
+  intake2.move(0);
+}*/
+
+
+void swing(int speed, int degrees, bool right) {
+  
+  if(right){
+  	
+  	chassis.set_swing_pid(ez::RIGHT_SWING, degrees, speed);
+  	chassis.wait_drive();
+  }
+  else{
+	chassis.set_swing_pid(ez::LEFT_SWING, degrees, speed);
+  	chassis.wait_drive();
+  }
+}
+
+void  turn(int speed, int degrees, bool right) {
+  
+  if(right){
+  	
+  	chassis.set_turn_pid(-degrees, speed);
+  	chassis.wait_drive();
+  }
+  else{
+	chassis.set_turn_pid(degrees, speed);
+  	chassis.wait_drive();
+  }
+}
+
+
+
+
 
 
 
