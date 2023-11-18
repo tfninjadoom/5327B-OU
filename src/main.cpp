1ea8d6f14757e3894909666af40e8133fce84472
amir-eftekhar <141464729+amir-eftekhar@users.noreply.github.com>
2023-11-17 20:40:32 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,36 @@ const int DRIVE_SPEED = 100;
 int INTAKE_DURATION{5000}; // duration of intake before stopping
 int INTAKE_SPEED{150}; // for the speed of the intakezzz
 // Function to grab the ball
-void moveForward(double inches,int speed) {
+void moveForward(double inches,int speed, bool wait=true) {
   chassis.set_drive_pid(inches*2, speed);
-  chassis.wait_drive();
+  if(wait){
+  	chassis.wait_drive();
+  }
+  else{
+	pros::delay(500);
+  
+  }
+  
+  //pros::delay(100);
+  //chassis.wait_drive();
 }
-void moveBackward(double inches, int speed) {
+void moveBackward(double inches, int speed, bool wait= 	true) {
   chassis.set_drive_pid(-inches*2, speed);
-  chassis.wait_drive();
+  if(wait){
+  	chassis.wait_drive();
+  }
+  else{
+	pros::delay(500);
+  
+  }
+
 }
 // the move parameter is if moveing forward would help grab the ball for example if the ball if against a barrier, the same for realeing the ball
 void grabBall(int speed, int duration, bool move, int dis) {
   intake.move(speed);
   intake2.move(-speed);
   if(move){
-    moveForward(dis,DRIVE_SPEED);
+    moveForward(dis,DRIVE_SPEED, true);
   }
   pros::delay(duration);
   intake.move(0);
@@ -41,7 +57,7 @@ void releaseBall(int speed, int duration,bool move, int dis) {
   intake.move(-speed);
   intake2.move(speed);
   if(move){
-    moveForward(dis,DRIVE_SPEED);
+    moveForward(dis,DRIVE_SPEED, true);
   }
   pros::delay(duration);
   intake.move(0);
@@ -54,8 +70,8 @@ void stopIntake(){
 }
 
 void startintake(int speed){
-  intake.move(speed);
-  intake2.move(-speed);
+  intake.move(-speed);
+  intake2.move(speed);
 }	
 
 void realease(int speed){
@@ -66,23 +82,137 @@ void  turn(int speed, int degrees, bool right) {
   
   if(right){
   	
-  	chassis.set_turn_pid(-degrees, speed);
+  	chassis.set_turn_pid(degrees, speed);
   	chassis.wait_drive();
   }
   else{
-	chassis.set_turn_pid(degrees, speed);
+	chassis.set_turn_pid(-degrees, speed);
+  	chassis.wait_drive();
+  }
+
+}
+void swing(int speed, int degrees, bool right) {
+  
+  if(right){
+  	
+  	chassis.set_swing_pid(ez::RIGHT_SWING, degrees, speed);
+  	chassis.wait_drive();
+  }
+  else{
+	chassis.set_swing_pid(ez::LEFT_SWING, degrees, speed);
   	chassis.wait_drive();
   }
 }
-void autonomousPath() {
+
+void extendWings(bool extendOrNot) {
+    //Wing::extendLeft(extendOrNot);
+    Wing::extendRight(extendOrNot);
+     
+    
+}
+void test(){
+	startintake(100);
+	moveForward(5, 100);
+	
+	pros::delay(100);
+	stopIntake();
+	moveBackward(42, 100);
+	turn(100, 180, true);
+	swing(100, 135, true);
+	moveForward(30, 127, false);
+	extendWings(true);
+	swing(100, 90, true);
+	//push
+	realease(127);
+	pros::delay(100);
+	moveForward(25, 127, false);
+	
+	moveBackward(15, 100, false);
+	moveForward(20, 127, false);
+	stopIntake();
+	moveBackward(15, 100);
+	extendWings(false);
+	imu.reset(90);
+	//turn(100, 45, false);
+	//imu.set_heading(90);
+	//chassis.set_angle(15);
+	turn(100, 70, false);
+	//swing(100, 0, false);
+	//imu.set_heading(45);
+	//pros::delay(1000);
+	
+	
+	moveForward(75, 100);
+	pros::delay(100);
+	startintake(100);
+
+	moveForward(5, 100);
+	pros::delay(100);
+	stopIntake();
+	moveBackward(10, 100);
+	extendWings(true);
+	swing(100, 90, false);
+	realease(100);
+	moveForward(30, 127);
+	//moveForward(5, 100);
+	
+
+}
+
+void fiveballAuton(){
+  startintake(127);
+  moveForward(7,100);
+  //pros::delay(200);
+  stopIntake();
+  moveBackward(35, 100);
+  turn(100, 180, true);
+  //extendboth(true);
+  moveForward(30, 100);
+  turn(100, 155, true);
+  realease(INTAKE_SPEED);
+  moveForward(20, 100);
+  turn(100, 10, true);
+  moveForward(40, 120);
+  moveBackward(20, 100);
+  turn(100, 60, false);
+
+  //extendboth(false);
+  moveForward(40, 75);
+  turn(100, 60, true);
+  startintake(100);
+  moveForward(30, 100);
+  stopIntake();
+  
+  moveBackward(10, 75);
+  /*turn(75, 75, false);
+  //extendboth(true);
+  moveForward(24, 75);
+  turn(75, 90, true);
+  realease(INTAKE_SPEED);
+  moveForward(35, 125);
+  pros::delay(300);
+  moveBackward(15, 75);
+  turn(75, 180, false);
+  startintake(INTAKE_SPEED);
+  moveForward(40, 75);
+  pros::delay(200);
+  stopIntake();
+  moveBackward(10, 75);
+  turn(75, 180, false);
+  moveForward(40, 125);
+  pros::delay(200);
+  moveBackward(10, 75);
+  */
+}
+void oneballAuton() {
  	moveForward(35, 100);
 	realease(100);
 	
 	pros::delay(500);
 	stopIntake();
 	moveBackward(15, 100);
-	turn(100, 165, true);
-	moveBackward(30, 100);
+	turn(100, 160, true);
+	moveBackward(35, 100);
 
   //moveForward(10,DRIVE_SPEED);
   //moveBackward(10,DRIVE_SPEED);
@@ -130,6 +260,8 @@ void on_center_button() {
  * and driver control. All other competition modes are blocked by initialize, 
  * so keep execution time for this mode under a few seconds.
  */
+
+
 void initialize() {
 
 
@@ -211,8 +343,8 @@ void autonomous() {
 
 	*/
 
-	
-	autonomousPath();
+	test();
+	//fiveballAuton();
 
 }
 
@@ -309,7 +441,7 @@ void opcontrol() {
 
 		// autonomous
 		if ( newPress(A) ) {
-			autonomousPath();
+			//1ballauton();
 		}
 
 
